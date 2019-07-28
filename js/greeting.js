f3668c514d6c4bf9e3f2da23ef2446873b782361
Konstantinos Oikonomou <kons.oikonomou@gmail.com>
2019-07-28 00:19:38 -0500

    changes

--- js/greeting.js
+++
@@ -1,9 +0,0 @@
-const greetings = ["Hey! This is . . .", "Hi there! I'm . . .",
-                 "Welcome! This is . . ."];
-
-function chooseGreeting() {
-  var index = Math.floor(Math.random()*greetings.length);
-  return greetings[index];
-}
-
-document.getElementById("greeting").innerHTML = chooseGreeting();
